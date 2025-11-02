229ab50b059845708ec501edcde2641f01339a82
aryalr <aryajati45@gmail.com>
2025-11-02 16:03:35 +0700

    update content web flask

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Halo, ini adalah proyek DevOps pertama saya!'
+    return 'Halo, ini adalah proyek DevOps pertama saya! saya telah push image ini ke dockerhub!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
